bda5d601d432b63861d9cfdae92b3a6e1160d0f1
bac bui <huyboken@gmail.com>
2022-09-05 16:44:01 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,12 @@ const ErrorHandler = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload")
-const dotenv = require("dotenv");
 const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(fileUpload({ useTempFiles: true }));
 
 // config
