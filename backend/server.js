6b34b941da7c38c5e673f47234190765d89e9f60
bac bui <huyboken@gmail.com>
2022-09-06 09:28:54 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const app = require("./app");
-const dotenv = require("dotenv");
 const connectDatabase = require("./db/Database.js");
 const cloudinary = require("cloudinary");
 
--- frontend/src/components/Admin/Sidebar.jsx
+++ frontend/src/components/Admin/Sidebar.jsx
@@ -11,8 +11,7 @@ import RateReviewIcon from "@material-ui/icons/RateReview";
 const Sidebar = () => {
 
     const button = () => {
-        let items = document.querySelectorAll(".Dashboard__item");
-
+        document.querySelectorAll(".Dashboard__item");
     }
 
     return (
